6f0162d6bd3ade65b7dda5ca8394f1ba292246da
meltbanana@yandex.ru <meltbanana@yandex.ru>
2020-05-04 22:35:22 +0300

    3/2 programmista (v2)

--- sandbox.py
+++ sandbox.py
@@ -4,7 +4,7 @@ end_dic.update({x: 'тов' for x in range(5,21)})
 
 try:
     x = int(input("Введите целое число программистов :"))
-    print (x,'программис'+end_dic[x%100] if x%100 in end_dic else end_dic[x%10])
+    print (x,"программис"+(end_dic[x%100] if x%100 in end_dic else end_dic[x%10]))
 
 except ValueError:
     print("Ошибка, нужно было ввести целое число, попробуйте еще раз.")
