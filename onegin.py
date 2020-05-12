371ab971a5cc9af245745fafdca43d795be59f2e
meltbanana@yandex.ru <meltbanana@yandex.ru>
2020-05-12 14:03:14 +0300

    Onegin 2.0

--- onegin.py
+++ onegin.py
@@ -1,6 +1,8 @@
 import re
 import collections
 import csv
+import matplotlib.pyplot as plt
+from math import log
 
 with open('onegin.txt', encoding='utf-8') as fsrc:
     words = re.findall(r'\w+', fsrc.read().lower())
@@ -12,3 +14,12 @@ with open('onegin.txt', encoding='utf-8') as fsrc:
         for key, value in dict(counter).items():
             writer.writerow([key, value])
         fdest.close()
+
+    frequences = sorted(dict(counter).values(), reverse = True)
+    x, y = zip(*enumerate(frequences, 1))
+    x = [log(i) for i in x]
+    y = [log(i) for i in y]
+
+    plt.plot(x, y)
+    plt.show()
+
